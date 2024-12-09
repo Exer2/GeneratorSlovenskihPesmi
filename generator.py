0cfe1baa5e80738ebe5495662c3d4c1ed0bd2e83
Exer2 <kukuc123.kukuc123@gmail.com>
2024-12-09 20:30:44 +0100

    naslov

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ API_KEY = os.getenv("API_KEY")
 def generator_pesmi(kljucna_beseda):
     client = InferenceClient(api_key=API_KEY)
     messages = [
-	    { "role": "user", "content": f"Napiši pesem v slovenščini s pomočjo ključne besede: {kljucna_beseda}. Napiši le pesem z naslovom. Pesem naj ima najmanj 1 in največ 4 kitice" }
+	    { "role": "user", "content": f"Napiši pesem v slovenščini s pomočjo ključne besede: {kljucna_beseda}. Izpiši le naslov in pod naslovom le pesem." }
     ]
     stream = client.chat.completions.create(
         model="Qwen/Qwen2.5-72B-Instruct", 
