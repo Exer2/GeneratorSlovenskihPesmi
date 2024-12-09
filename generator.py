6ecec4453a11543651a75e4cae8754faf3924089
Exer2 <kukuc123.kukuc123@gmail.com>
2024-12-09 17:16:40 +0100

    slovenija detect

--- generator.py
+++ generator.py
@@ -2,7 +2,25 @@ from huggingface_hub import InferenceClient
 #from config import API_KEY
 import os
 
+from langdetect import detect
+
+
 API_KEY = os.getenv("API_KEY")
+
+
+
+def preveri_jezik(stavek):
+    return detect(stavek) == "sl"
+
+def odstrani_neslovenske_besede(besedilo):
+    vrstice = besedilo.split("\n")
+    filtrirane_vrstice = []
+    for vrstica in vrstice:
+        besede = vrstica.split()
+        filtrirane_besede = [beseda for beseda in besede if preveri_jezik(beseda)]
+        filtrirane_vrstice.append(" ".join(filtrirane_besede))
+    return "\n".join(filtrirane_vrstice)
+
 
 def generator_pesmi(kljucna_beseda):
     client = InferenceClient(api_key=API_KEY)
