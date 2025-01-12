9f9d82d9022d8192ef99fb03ad91d1e7a2acaffd
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 21:01:08 +0100

    adf

--- vmesnik.py
+++ vmesnik.py
@@ -1,22 +1,26 @@
 import streamlit as st
 from generator import generator_pesmi
 
-
-
-# Uporabniški vmesnik V1
-
 # Funkcija za generiranje pesmi
 def generate_poem(word):
     pesem = generator_pesmi(word)
     return pesem
-    
-st.title("Pesmopisec")
-word = st.text_input("Vpišite ključno besedo:")
 
+st.title("Pesmopisec")
+
+# Postavimo polje za vnos in gumb v isti vrstici
+col1, col2 = st.columns([4, 1])  # Določimo širino stolpcev (4:1)
+
+with col1:
+    word = st.text_input("Vpišite ključno besedo:")
+
+with col2:
+    if st.button("Gumb"):
+        st.info("Pritisnili ste gumb!")
 
 if st.button("Generiraj pesem"):
     if word:
         poem = generate_poem(word)
-        st.text_area("Vaša pesem:", poem, height=500)       
+        st.text_area("Vaša pesem:", poem, height=500)
     else:
         st.error("Vnesite ključno besedo!")
