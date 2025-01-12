06ed310c09bd942e9ba0d6e8ca5fbd8876ed530b
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 18:50:58 +0100

    asdf

--- generator.py
+++ generator.py
@@ -1,36 +1,10 @@
-from huggingface_hub import InferenceClient
 import os
 import google.generativeai
 
 API_KEY = os.getenv("API_KEY")
 google.generativeai.configure(api_key=API_KEY)
 
-"""def generator_pesmi(kljucna_beseda):
-    client = InferenceClient(api_key=API_KEY)
-    messages = [
-	    { "role": "user", "content": f"Napiši pesem v slovenščini s pomočjo ključne besede: {kljucna_beseda}. Izpiši le naslov in pod naslovom le pesem." }
-    ]
-    stream = client.chat.completions.create(
-        model="Qwen/Qwen2.5-72B-Instruct", 
-        messages=messages, 
-        temperature=0.9,
-        top_p=0.9,
-        max_tokens=200,
-        stream=True
-    )
-
-    pesem = ""
-
-    for chunk in stream:
-        content = chunk.choices[0].delta.content
-        if content:  # Preverimo, ali je del besedila prazen
-            pesem += content
-
-    return pesem
-    """
 def generator_pesmi(kljucna_beseda):
-    
-
     model = google.generativeai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Explain how AI works")
+    response = model.generate_content(f"Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. Poskusi uporabiti najboljše rime, kot jih lahko.")
     return response.text
