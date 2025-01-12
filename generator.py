4c69dba8ba727e526b4fa7d513cf3ccf6ab26153
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 19:16:12 +0100

    asdf

--- generator.py
+++ generator.py
@@ -6,6 +6,5 @@ google.generativeai.configure(api_key=API_KEY)
 
 def generator_pesmi(kljucna_beseda):
     model = google.generativeai.GenerativeModel("gemini-2.0-flash-exp")
-    response = model.generate_content(f"Prevzami vlogo pesnika, pisatelja pesmi. Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. 
-    uporabljaj rime, kot jih slovenski pesniki uporabljajo v svojih pesmih.")
+    response = model.generate_content(f"Prevzami vlogo pesnika, pisatelja pesmi. Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. Uporabljaj rime, kot jih slovenski pesniki uporabljajo v svojih pesmih, kot na primer verižna rima, zaporedna rima, končna rima, prestopna rima itd.")
     return response.text
