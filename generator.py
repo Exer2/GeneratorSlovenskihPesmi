809c757f53b0afef1335c7c643c303313ae02ef5
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 19:14:44 +0100

    asdf

--- generator.py
+++ generator.py
@@ -6,5 +6,6 @@ google.generativeai.configure(api_key=API_KEY)
 
 def generator_pesmi(kljucna_beseda):
     model = google.generativeai.GenerativeModel("gemini-2.0-flash-exp")
-    response = model.generate_content(f"Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. Poskusi uporabiti najboljše rime, kot jih lahko.")
+    response = model.generate_content(f"Prevzami vlogo pesnika, pisatelja pesmi. Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. 
+    uporabljaj rime, kot jih slovenski pesniki uporabljajo v svojih pesmih.")
     return response.text
