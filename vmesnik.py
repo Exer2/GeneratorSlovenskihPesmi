bd7c97acd78edf4e575c024f5c26f9fbf13b2178
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 21:59:31 +0100

    23

--- vmesnik.py
+++ vmesnik.py
@@ -11,7 +11,7 @@ st.title("Pesmopisec")
 
 # Postavitev z napisom in poljem za vnos
 #st.write("Vpišite ključno besedo:")
-col1, col2 = st.columns([4, 3])
+col1, col2 = st.columns([2, 1])
 
 with col1:
     word = st.text_input("bla", label_visibility="collapsed", placeholder="Vnesite ključno besedo...")
