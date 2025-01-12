9cb30df3937ae44deca376b3430d686be9760946
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 20:59:56 +0100

    asdf

--- vmesnik.py
+++ vmesnik.py
@@ -11,29 +11,12 @@ def generate_poem(word):
     return pesem
     
 st.title("Pesmopisec")
-
-navodila = "Vpišite ključno besedo:"
-
-with st.form('chat_input_form'):
-    # Create two columns; adjust the ratio to your liking
-    col1, col2 = st.columns([3,1]) 
-
-    # Use the first column for text input
-    with col1:
-        k_beseda = st.text_input(
-            value=navodila
-            )
-    with col2:
-        posnemi_zvok = st.form_submit_button('Snemaj')
-    
-    if k_beseda and posnemi_zvok:
-        # Do something with the inputted text here
-        st.write(f"You said: {k_beseda}")
+word = st.text_input("Vpišite ključno besedo:")
 
 
 if st.button("Generiraj pesem"):
-    if k_beseda:
-        poem = generate_poem(k_beseda)
+    if word:
+        poem = generate_poem(word)
         st.text_area("Vaša pesem:", poem, height=500)       
     else:
         st.error("Vnesite ključno besedo!")
