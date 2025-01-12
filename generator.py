03b2a7b60c580f402846a29ae83493cc9e284ea3
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 18:56:19 +0100

    asdf

--- generator.py
+++ generator.py
@@ -5,6 +5,6 @@ API_KEY = os.getenv("API_KEY")
 google.generativeai.configure(api_key=API_KEY)
 
 def generator_pesmi(kljucna_beseda):
-    model = google.generativeai.GenerativeModel("gemini-1.5-pro")
+    model = google.generativeai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(f"Napiši kratko pesem v slovenščini glede na ključno besedo: {kljucna_beseda}. Poskusi uporabiti najboljše rime, kot jih lahko.")
     return response.text
