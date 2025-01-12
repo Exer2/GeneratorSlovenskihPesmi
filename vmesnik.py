9c65280c525c2e9c7fb8538335ba63e2319a0705
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 20:30:27 +0100

    adsf

--- vmesnik.py
+++ vmesnik.py
@@ -3,18 +3,25 @@ from generator import generator_pesmi
 
 
 
-# Uporabniški vmesnik V1
+# Začnite z vključitvijo Font Awesome
+st.markdown(
+    """
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    """, unsafe_allow_html=True
+)
 
 # Funkcija za generiranje pesmi
 def generate_poem(word):
+    # Tu boš vključil logiko za generiranje pesmi
     pesem = generator_pesmi(word)
     return pesem
-    
+
+# Nastavitve za Streamlit
 st.title("Pesmopisec")
 word = st.text_input("Vpišite ključno besedo:")
 
-
-if st.button("Generiraj pesem"):
+# Gumb z ikono
+if st.button("🔴 Generiraj pesem"):  # Ikona rdeče krogle za snemanje
     if word:
         poem = generate_poem(word)
         st.text_area("Vaša pesem:", poem, height=500)       
