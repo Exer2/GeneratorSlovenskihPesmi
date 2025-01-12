9bac16b2540b57d878c18d1d873d0347ae562342
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 18:47:28 +0100

    asdf

--- generator.py
+++ generator.py
@@ -1,9 +1,11 @@
 from huggingface_hub import InferenceClient
 import os
+import google.generativeai
 
 API_KEY = os.getenv("API_KEY")
+google.generativeai.configure(api_key=API_KEY)
 
-def generator_pesmi(kljucna_beseda):
+"""def generator_pesmi(kljucna_beseda):
     client = InferenceClient(api_key=API_KEY)
     messages = [
 	    { "role": "user", "content": f"Napiši pesem v slovenščini s pomočjo ključne besede: {kljucna_beseda}. Izpiši le naslov in pod naslovom le pesem." }
@@ -25,3 +27,10 @@ def generator_pesmi(kljucna_beseda):
             pesem += content
 
     return pesem
+    """
+    def generator_pesmi(kljucna_beseda):
+        
+
+        model = google.generativeai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content("Explain how AI works")
+        return response.text
