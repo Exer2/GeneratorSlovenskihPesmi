c12b2d71392838d4911b3b36615daac8d5533de3
Exer2 <kukuc123.kukuc123@gmail.com>
2025-01-12 21:36:44 +0100

    qwer

--- vmesnik.py
+++ vmesnik.py
@@ -31,7 +31,7 @@ with col2:
         justify-content: center; /* Poravnava vodoravno */
         width: 40px;
         height: 40px; /* Višina gumba nastavljena na 40px */
-        margin-top: -7px; /* Poravnava glede na vnosno polje */
+        margin-top: -9px; /* Poravnava glede na vnosno polje */
     }
     .icon-button svg {
         width: 24px;
